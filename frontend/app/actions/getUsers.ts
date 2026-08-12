84c4dc15ca7d67e6179d4867290c5588519522b0
hoangtubongdem1511 <92940233+hoangtubongdem1511@users.noreply.github.com>
2026-08-12 16:24:48 +0700

    fix import

--- frontend/app/actions/getUsers.ts
+++ frontend/app/actions/getUsers.ts
@@ -1,5 +1,5 @@
 import { serverGet } from '@/app/lib/server-api-client';
-import { User } from '@prisma/client';
+import { User } from '../types/user';
 
 const getUsers = async (): Promise<User[]> => {
     try {
